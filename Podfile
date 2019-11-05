6f2dc4ec28db9c1408b9047782af6496799546c6
Alexey Strakh <alexey.strakh@microsoft.com>
2019-11-05 14:25:22 -0800

    sample test IoTHubFramework

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+workspace 'testIoTHubFramework'
+target 'testIoTHubFramework' do
+  use_frameworks!
+  pod 'AzureIoTHubClient'
+end
+
---
+++ testIoTHubFramework/testIoTHubFramework.h
@@ -0,0 +1,19 @@
+//
+//  testIoTHubFramework.h
+//  testIoTHubFramework
+//
+//  Created by Alexey Strakh on 11/5/19.
+//  Copyright © 2019 Xamarin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for testIoTHubFramework.
+FOUNDATION_EXPORT double testIoTHubFrameworkVersionNumber;
+
+//! Project version string for testIoTHubFramework.
+FOUNDATION_EXPORT const unsigned char testIoTHubFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testIoTHubFramework/PublicHeader.h>
+
+
